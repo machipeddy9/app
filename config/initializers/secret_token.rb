305fa08e3f99b8a4512057b70d5fa85934660684
machipeddy9@gmail.comgit config --global user.email machipeddy9@gmail.com <machipeddy9@gmail.com>
2013-04-23 18:50:13 +0530

    first_app code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '48dd8659bd5be05e2ab12d429f3a4880b8c78a7f9cde3709798238b7db87b1715d197328363e42ce04171ce75968950c8d3466205f9c0e348a64d3affc5f5b28'
